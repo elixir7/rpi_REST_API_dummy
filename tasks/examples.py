a1176b3bb75f5ff8945202a0fd4caf10aaa658cd
Isak Åslund <aslundisak@gmail.com>
2019-08-09 17:49:25 +0200

    Saturation fix + printer factory reset and setup

--- tasks/examples.py
+++ tasks/examples.py
@@ -26,9 +26,14 @@ def changeColor():
     tries = 0
     while tries < 3:
         hue_input = prompt.query("Enter hue (0-360): ")
+        sat_input = prompt.query("Enter saturation (0-100): ")
+        brightness_input = prompt.query("Enter brightness (0-100): ")
         try:
             hue = int(hue_input)
-            puts("Setting hue to: " + str(hue))
+            saturation = int(sat_input)
+            brightness = int(brightness_input)
+
+            puts("Setting hue: " + str(hue) + ", saturation: " + str(saturation) + ", brightness: " + str(brightness))
 
             printers_json = json.load(open("printers.json", "rt"))
             printers = list()
@@ -38,7 +43,12 @@ def changeColor():
                 printers.append(printer)
             
             for printer in printers:
-                printer.put("api/v1/printer/led", data={"brightness": 100.0, "saturation": 100.0, "hue": hue})
+                systemInfo = printer.get("api/v1/system").json()
+                if systemInfo['variant'] == "Ultimaker S5":
+                    #The UM S5 does not have rgb lightning...  and saturation is inverted so only change the brightness
+                    printer.put("api/v1/printer/led", data={"brightness": brightness, "saturation": 0, "hue": hue})
+                else:
+                    printer.put("api/v1/printer/led", data={"brightness": brightness, "saturation": saturation, "hue": hue})
                 #if "UMS5" in printer.getName():
                     #UMS5 has inverted saturation for some reason
                 #    printer.put("api/v1/printer/led", data={"brightness": 100.0, "saturation": 0, "hue": hue})
