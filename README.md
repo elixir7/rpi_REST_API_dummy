953a7e42ee99635f8c2d105e12ca08bdf1cdd2ae
Isak Åslund <aslundisak@gmail.com>
2019-08-10 00:45:55 +0200

    Request exception handling + Docstring documentation on printer.py

--- README.md
+++ README.md
@@ -38,17 +38,13 @@ $ python3 app.py
 ### TODO
 If not all printers are connected to the network the system will not work since the program will try to send requests to an IP that has no reciever. This needs to be fixed ASAP.
 
-### Ultimaker 3  JSON Data
-This is just used for writing down data that will be needed later.
-{
-    "name": "CASE-Lab-UM3",
-    "ip": "192.168.1.201",
-    "id": "e4a69cf7286b414e1f85206851fecb93",
-    "key": "38cd2e19fce0533e3f66c5dae1df84bdbea0a1d9c2779cb5118dd3be55f2c186"
-},
-{
-    "name": "CASE-Lab-UMS5",
-    "ip": "192.168.1.242",
-    "id": "b8bad80c28abe2812342285d08a20d08",
-    "key": "db0190c07220cd7927b9403c21c14a4b00b1a4f60332ebb7de632e8d4d1594c4"
-}
+### Useful commands
+In order to kill a process running on a ceratin port you can use the following commands.
+Make suer you got **lsof** installed, if not run "sudo apt install lsof"
+List processes running on a port:
+"lsof -i:port" => "lsof -i:8080" lists the port on port 8080.
+
+You can then kill them by using:
+"kill PID" PID = process ID.
+If that doesn't work you can use the flag -9 to force it.
+"kill -9 PID" will kill the process.
--- printer.py
+++ printer.py
@@ -4,19 +4,22 @@ import time
 from clint.textui import puts, indent, colored, prompt
 from getpass import getuser
 
-#####       _ = private method
-
 class Printer:
 
     #When creating a instance, data must be a legit ip or a dict with the info of the printer
     def __init__(self, data):
-        #Let the type of data decide what to do. 
+        """ Create a printer object
+            
+        
+        Arguments:
+            data {string or dict} -- Let the type of data decide what to do. String => Setup a new printer with auth. Dict => Authenticate and connect using id and key.
+        """
         if(isinstance(data, str)):
             puts(colored.cyan("Created Printer, will now try to connect..."))
             puts(colored.cyan("Go over to the printer and click allow!"))
             self._ip = data
             self._application = "CASE-RPI Printer Request"
-            self._session = requests.sessions.Session()
+            self._session = requests.Session()
             self._setAuthData("", "")
 
             self._checkAuth()
@@ -24,14 +27,18 @@ class Printer:
             response = self.get("api/v1/system/name")
             self._name = response.json()
 
-        elif(isinstance(data, dict)): 
-            puts("Connected to printer - " + colored.cyan(data["name"]))        
+        elif(isinstance(data, dict)):        
             self._ip = data["ip"]
             self._name = data["name"]
-            self._session = requests.sessions.Session()
+            self._session = requests.Session()
             self._setAuthData(data["id"], data["key"])
 
-            self._checkAuth()
+            
+            if(not self._checkAuth()):
+                puts("Check Auth failed when trying to create printer from dict data")
+                return
+
+            puts("Connected to printer - " + colored.cyan(data["name"]))   
 
             response = self.get("api/v1/system/name")
             self._name = response.json()
@@ -42,19 +49,29 @@ class Printer:
         self._auth = requests.auth.HTTPDigestAuth(self._auth_id, self._auth_key)
 
     def _checkAuth(self):
+        """ Verifies that the printer object is valid and authenticated by checking that id exists and that a request and response is successful.
+        
+        Raises:
+            RuntimeError: If the authorization is unautorized.
+        
+        Returns:
+            bool -- True if the authentification was valid, othervise false.
+        """
         if self._auth_id == "" or self.get("api/v1/auth/verify").status_code != 200:
-            puts(colored.yellow("No auth or auth check failed, requesting new authentication"))
-            response = self.post("api/v1/auth/request", data={"application": self._application, "user": getuser()})
-            if response.status_code != 200:
-                puts(colored.red("Failed to request new API key"))
-                raise RuntimeError("Failed to request new API key")
+            puts(colored.yellow("No authentification ID or auth check failed"))
+            puts("Requesting new authentification ID and KEY")
+            r = self.post("api/v1/auth/request", data={"application": self._application, "user": getuser()})
+
             data = response.json()
             self._setAuthData(data["id"], data["key"])
-            while True:
-                time.sleep(1)
+
+
+            puts("Trying to get authenticated by the printer, you have 60s to press ACCEPT on the printer")
+            while tries <= 60:
+                puts(colored.cyan("Waiting for authorization from printer..."))
                 response = self.get("api/v1/auth/check/%s" % (self._auth_id))
                 data = response.json()
-                puts("Waiting for authorization from printer...")
+
                 with indent(4):
                     puts("Message: " + data["message"])
                 if data["message"] == "authorized":
@@ -62,7 +79,11 @@ class Printer:
                     return True
                 if data["message"] == "unauthorized":
                     raise RuntimeError("Authorization denied")
-            return False
+
+                tries++
+                time.sleep(1)
+            # No accepted authentification from the printer after 60s
+            return False    
         return True
 
     def getName(self):
@@ -77,8 +98,12 @@ class Printer:
     def getKey(self):
         return self._auth_key
     
-    #Used for saving down to a file
     def getPrinterAsDict(self):
+        """ Used for saving down to a file
+        
+        Returns:
+            dict -- A dict containing name, ip, id and key.
+        """
         printer = {
             "name": self._name,
             "ip": self._ip,
@@ -87,22 +112,71 @@ class Printer:
         }
         return printer
 
-    # Do a new HTTP request to the printer. It formats data as JSON, and fills in the IP part of the URL.
     def request(self, method, path, **kwargs):
+        """ Send a request (get, put, post) to the printer for e.g chaning the lightning or getting printer status.
+        
+        Arguments:
+            method {string} -- What type of request ("get", "put", "post")
+            path {string} -- Path for request, the part after http://IP/ e.g. "api/v1/printer/led"
+
+        Returns:
+            Response Object -- Returns a Response object containing the returned information and the originall Request object.
+        """
+
         if "data" in kwargs:
             kwargs["data"] = json.dumps(kwargs["data"])
             if "headers" not in kwargs:
                 kwargs["headers"] = {"Content-type": "application/json"}
-        return self._session.request(method, "http://%s/%s" % (self._ip, path), auth=self._auth, **kwargs)
 
-    # Shorthand function to do a "GET" request.
+
+        # What should happen if an exception was raised and what should be returned?????
+        # Timeout could perhaps make a authentification request fail because you don't press "accept" on the ultimaker within 7s
+        try:
+            # Attempt to make a request, raise a Timeout Exception if it takes to long and a HTTP error if the response code is 4XX or 5XX.
+            r = self._session.request(method, "http://%s/%s" % (self._ip, path), auth=self._auth, timeout=7.0,  **kwargs).raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            puts(colored.red("Http Error:" + err))
+            return
+        except requests.exceptions.ConnectionError as err:
+            puts(colored.red("Error Connecting:" + err))
+            return
+        except requests.exceptions.Timeout as err:
+            puts(colored.red("Timeout Error:" + err))
+            return
+        except requests.exceptions.RequestException as err:
+            puts(colored.red("Request Error:" + err))
+            return
+        return r
+
     def get(self, path, **kwargs):
+        """ Shorthand function to do a "GET" request.
+        
+        Arguments:
+            path {string} -- Path for request, the part after http://IP/ e.g. "api/v1/printer/led"
+        
+        Returns:
+             Response Object -- Returns a Response object containing the returned information and the originall Request object.
+        """
         return self.request("get", path, **kwargs)
 
-    # Shorthand function to do a "PUT" request.
     def put(self, path, **kwargs):
+        """ Shorthand function to do a "PUT" request.
+        
+        Arguments:
+            path {string} -- Path for request, the part after http://IP/ e.g. "api/v1/printer/led"
+        
+        Returns:
+             Response Object -- Returns a Response object containing the returned information and the originall Request object.
+        """
         return self.request("put", path, **kwargs)
 
-    # Shorthand function to do a "POST" request.
     def post(self, path, **kwargs):
+        """ Shorthand function to do a "POST" request.
+        
+        Arguments:
+            path {string} -- Path for request, the part after http://IP/ e.g. "api/v1/printer/led"
+        
+        Returns:
+             Response Object -- Returns a Response object containing the returned information and the originall Request object.
+        """
         return self.request("post", path, **kwargs)
